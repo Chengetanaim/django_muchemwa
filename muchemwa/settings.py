d146aa7ee5a42351d6f4751780f172d1c9756375
Chengetanai <mukanhairic@gmail.com>
2021-08-08 12:16:48 -0700

    eighth commit

--- muchemwa/settings.py
+++ muchemwa/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     # https://warehouse.python.org/project/whitenoise/
 
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
