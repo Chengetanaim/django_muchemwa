07b37250a0c18cfeef3ce9686df13405b4fe4856
Chengetanai <mukanhairic@gmail.com>
2021-08-08 09:32:52 -0700

    second commit

--- muchemwa/settings.py
+++ muchemwa/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-9r=(g4(z@vj5#i^r@jykex3wf5xc75-4se*(f-s$&!+uc*7e*e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'django-muchemwa.hostman.site'
+]
 
 
 # Application definition
